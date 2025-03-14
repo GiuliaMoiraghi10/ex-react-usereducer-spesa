00b119b01ab8a77889454e9b0918106b50fa2324
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2025-03-14 18:05:45 +0100

    Milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-
 import './App.css'
 
 function App() {
 
+  const products = [
+    { name: 'Mela', price: 0.5 },
+    { name: 'Pane', price: 1.2 },
+    { name: 'Latte', price: 1.0 },
+    { name: 'Pasta', price: 0.7 },
+  ];
+
   return (
     <>
-
+      <h1>Lista della spesa</h1>
+      <ul>
+        {products.map((prodotto, i) => (
+          <li key={i}>
+            <h3>{prodotto.name}: {`${prodotto.price.toFixed(2)} €`}</h3>
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
