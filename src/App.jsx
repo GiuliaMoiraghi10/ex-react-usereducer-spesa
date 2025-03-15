d34da96483f2fc1cc770e0a28cfc8037cfac549e
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2025-03-15 14:56:21 +0100

    Milestone 3

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,17 @@ function App() {
   const [addedProducts, setAddedProducts] = useState([]) // variabile di stato x aggiungere el nel carrello
   console.log(addedProducts)
 
+  const updateProductQuantity = (name, quantity) => {// deve modificare setAddedProduct, deve cambiare la quantità
+    setAddedProducts(curr =>
+      curr.map(p => p.name === name ? { ...p, quantity } : p))
+  }
+
   const addToCart = product => { // questa funzione deve capire se c'è già un prodotto uguale aggiunto al carrello
-    const isProductAlreadyAdded = addedProducts.some(p => p.name === product.name);
-    if (isProductAlreadyAdded) {
+    // const isProductAlreadyAdded = addedProducts.some(p => p.name === product.name);
+
+    const alreadyAddedProduct = addedProducts.find(p => p.name === product.name)
+    if (alreadyAddedProduct) {
+      updateProductQuantity(alreadyAddedProduct.name, alreadyAddedProduct.quantity + 1)
       return;
     }
     const productToAdd = { // aggiungo il prodotto, tutto il prodotto corrente + la quantità
@@ -25,6 +33,12 @@ function App() {
     }
     setAddedProducts(curr => [...curr, productToAdd])
   }
+
+  const removeFromCart = product => {
+    setAddedProducts(curr => curr.filter(p => p.name !== product.name)) // mantengo all'interno del carrello tutti gli elementi che non hanno lo stesso nome
+  }
+
+  const totalToPay = addedProducts.reduce((acc, p) => acc + (p.price * p.quantity), 0)
 
   return (
     <>
@@ -43,9 +57,11 @@ function App() {
           {addedProducts.map((p, i) => (
             <li key={i}>
               <h4>{p.quantity} x {p.name} ({p.price.toFixed(2)}€)</h4>
+              <button onClick={() => removeFromCart(p)}>Rimuovi dal carrello</button>
             </li>
           ))}
         </ul>
+        <h2>Totale da pagare: {totalToPay.toFixed(2)}€</h2>
       </>)}
     </>
   )
